458dcd28740e2b1318fa37147908bfef550b9364
Alexandru Iustin Burlacu <alexandru.burlacu@external.ivecogroup.com>
2024-07-25 09:34:37 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+"use client"
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.main}>
-      Hello World
-    </div>
-  );
+    const [counter, setCounter] = useState(0);
+
+    const onClickHandler = (type: string) => {
+        if(type === '+') {
+            setCounter(counter + 1);
+        } else if(type === '-') {
+            setCounter(counter - 1);
+        }
+    }
+
+    return (
+        <div className={styles.main}>
+          Hello World
+            <div className={styles.divButton}>
+                <button className={styles.button} onClick={() => onClickHandler('+')}> +</button>
+                {counter}
+                <button className={styles.button} onClick={() => onClickHandler('-')}> -</button>
+            </div>
+        </div>
+    );
 }
